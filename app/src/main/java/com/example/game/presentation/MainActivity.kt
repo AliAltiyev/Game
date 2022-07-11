81c6f31f23222d75c6ae18997879bbb790640705
AliAltiyev <alidroid696@gmail.com>
2022-07-11 22:50:36 +0300

    initial project

---
+++ app/src/main/java/com/example/game/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.game.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.game.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
